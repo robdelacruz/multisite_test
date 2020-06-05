03deb8819057e177978a4b1df7910f763723697c
rob <robdelacruz@gmail.com>
2020-06-05 12:24:03 +0800

    scaffolding

--- t2.go
+++ t2.go
@@ -1,6 +1,251 @@
 package main
 
-import ()
+import (
+	"database/sql"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"strings"
+
+	_ "github.com/mattn/go-sqlite3"
+)
 
 func main() {
+	os.Args = os.Args[1:]
+	sw, parms := parseArgs(os.Args)
+
+	// [-i new_file]  Create and initialize db file
+	if sw["i"] != "" {
+		dbfile := sw["i"]
+		if fileExists(dbfile) {
+			s := fmt.Sprintf("File '%s' already exists. Can't initialize it.\n", dbfile)
+			fmt.Printf(s)
+			os.Exit(1)
+		}
+		createTables(dbfile)
+		os.Exit(0)
+	}
+
+	// Need to specify a db file as first parameter.
+	if len(parms) == 0 {
+		s := `Usage:
+
+Start webservice using notes database file:
+	mn <notes.db>
+
+Initialize new notes database file:
+	mn -i <notes.db>
+`
+		fmt.Printf(s)
+		os.Exit(0)
+	}
+
+	// Exit if db file doesn't exist.
+	dbfile := parms[0]
+	if !fileExists(dbfile) {
+		s := fmt.Sprintf(`Notes database file '%s' doesn't exist. Create one using:
+	wb -i <notes.db>
+`, dbfile)
+		fmt.Printf(s)
+		os.Exit(1)
+	}
+
+	db, err := sql.Open("sqlite3", dbfile)
+	if err != nil {
+		fmt.Printf("Error opening '%s' (%s)\n", dbfile, err)
+		os.Exit(1)
+	}
+
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("./static"))))
+	http.HandleFunc("/favicon.ico", func(w http.ResponseWriter, r *http.Request) { http.ServeFile(w, r, "./static/coffee.ico") })
+	http.HandleFunc("/", indexHandler(db))
+
+	port := "8000"
+	fmt.Printf("Listening on %s...\n", port)
+	err = http.ListenAndServe(fmt.Sprintf(":%s", port), nil)
+	log.Fatal(err)
+}
+
+func sqlstmt(db *sql.DB, s string) *sql.Stmt {
+	stmt, err := db.Prepare(s)
+	if err != nil {
+		log.Fatalf("db.Prepare() sql: '%s'\nerror: '%s'", s, err)
+	}
+	return stmt
+}
+
+func sqlexec(db *sql.DB, s string, pp ...interface{}) (sql.Result, error) {
+	stmt := sqlstmt(db, s)
+	defer stmt.Close()
+	return stmt.Exec(pp...)
+}
+
+func txstmt(tx *sql.Tx, s string) *sql.Stmt {
+	stmt, err := tx.Prepare(s)
+	if err != nil {
+		log.Fatalf("tx.Prepare() sql: '%s'\nerror: '%s'", s, err)
+	}
+	return stmt
+}
+
+func txexec(tx *sql.Tx, s string, pp ...interface{}) (sql.Result, error) {
+	stmt := txstmt(tx, s)
+	defer stmt.Close()
+	return stmt.Exec(pp...)
+}
+
+func createTables(newfile string) {
+	if fileExists(newfile) {
+		s := fmt.Sprintf("File '%s' already exists. Can't initialize it.\n", newfile)
+		fmt.Printf(s)
+		os.Exit(1)
+	}
+
+	db, err := sql.Open("sqlite3", newfile)
+	if err != nil {
+		fmt.Printf("Error opening '%s' (%s)\n", newfile, err)
+		os.Exit(1)
+	}
+
+	ss := []string{
+		"CREATE TABLE user (user_id INTEGER PRIMARY KEY NOT NULL, username TEXT, password TEXT, active INTEGER NOT NULL, email TEXT, CONSTRAINT unique_username UNIQUE (username));",
+		"INSERT INTO user (user_id, username, password, active, email) VALUES (1, 'admin', '', 1, '');",
+	}
+
+	tx, err := db.Begin()
+	if err != nil {
+		log.Printf("DB error (%s)\n", err)
+		os.Exit(1)
+	}
+	for _, s := range ss {
+		_, err := txexec(tx, s)
+		if err != nil {
+			tx.Rollback()
+			log.Printf("DB error (%s)\n", err)
+			os.Exit(1)
+		}
+	}
+	err = tx.Commit()
+	if err != nil {
+		log.Printf("DB error (%s)\n", err)
+		os.Exit(1)
+	}
+}
+
+func parseArgs(args []string) (map[string]string, []string) {
+	switches := map[string]string{}
+	parms := []string{}
+
+	standaloneSwitches := []string{}
+	definitionSwitches := []string{"i", "import"}
+	fNoMoreSwitches := false
+	curKey := ""
+
+	for _, arg := range args {
+		if fNoMoreSwitches {
+			// any arg after "--" is a standalone parameter
+			parms = append(parms, arg)
+		} else if arg == "--" {
+			// "--" means no more switches to come
+			fNoMoreSwitches = true
+		} else if strings.HasPrefix(arg, "--") {
+			switches[arg[2:]] = "y"
+			curKey = ""
+		} else if strings.HasPrefix(arg, "-") {
+			if listContains(definitionSwitches, arg[1:]) {
+				// -a "val"
+				curKey = arg[1:]
+				continue
+			}
+			for _, ch := range arg[1:] {
+				// -a, -b, -ab
+				sch := string(ch)
+				if listContains(standaloneSwitches, sch) {
+					switches[sch] = "y"
+				}
+			}
+		} else if curKey != "" {
+			switches[curKey] = arg
+			curKey = ""
+		} else {
+			// standalone parameter
+			parms = append(parms, arg)
+		}
+	}
+
+	return switches, parms
+}
+
+func listContains(ss []string, v string) bool {
+	for _, s := range ss {
+		if v == s {
+			return true
+		}
+	}
+	return false
+}
+
+func fileExists(file string) bool {
+	_, err := os.Stat(file)
+	if err != nil && os.IsNotExist(err) {
+		return false
+	}
+	return true
+}
+
+// Helper function to make fmt.Fprintf(w, ...) calls shorter.
+// Ex.
+// Replace:
+//   fmt.Fprintf(w, "<p>Some text %s.</p>", str)
+//   fmt.Fprintf(w, "<p>Some other text %s.</p>", str)
+// with the shorter version:
+//   P := makeFprintf(w)
+//   P("<p>Some text %s.</p>", str)
+//   P("<p>Some other text %s.</p>", str)
+func makeFprintf(w io.Writer) func(format string, a ...interface{}) (n int, err error) {
+	return func(format string, a ...interface{}) (n int, err error) {
+		return fmt.Fprintf(w, format, a...)
+	}
+}
+
+func printHead(w io.Writer, jsurls []string, cssurls []string, title string) {
+	P := makeFprintf(w)
+	P("<!DOCTYPE html>\n")
+	P("<html>\n")
+	P("<head>\n")
+	P("<meta charset=\"utf-8\">\n")
+	P("<meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">\n")
+	P("<title>%s</title>\n", title)
+	P("<link rel=\"stylesheet\" type=\"text/css\" href=\"/static/style.css\">\n")
+	for _, cssurl := range cssurls {
+		P("<link rel=\"stylesheet\" type=\"text/css\" href=\"%s\">\n", cssurl)
+	}
+	for _, jsurl := range jsurls {
+		P("<script src=\"%s\" defer></script>\n", jsurl)
+	}
+	P("</head>\n")
+	P("<body>\n")
+}
+
+func printFoot(w io.Writer) {
+	P := makeFprintf(w)
+	P("</body>\n")
+	P("</html>\n")
+}
+
+func indexHandler(db *sql.DB) func(http.ResponseWriter, *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/html")
+		printHead(w, nil, nil, "Title")
+		//printNav(w, r, db, login, b, pageid)
+
+		P := makeFprintf(w)
+		P("<section class=\"main-container\">\n")
+		P("  <section class=\"flex flex-row justify-center\">\n")
+		P("  </section>\n")
+		P("</section>\n")
+		printFoot(w)
+	}
 }
